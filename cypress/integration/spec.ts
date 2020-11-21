5068401036c8633a3fc6457c5a3c99dfe0fc51e2
pehtoorismies <pehtoorismies@gmail.com>
2020-11-21 13:11:11 +0200

    Add cypress

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,8 @@
+describe("Dummy test", () => {
+  it("navigates", () => {
+    // find more Cypress commands at
+    // https://on.cypress.io/api
+    cy.visit("/");
+    cy.contains("Welcome to Next.js!");
+  });
+});
